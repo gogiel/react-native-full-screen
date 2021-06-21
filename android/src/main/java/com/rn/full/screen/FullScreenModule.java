ac9269e508288eab76b35139f267657879a6f5a4
Jan Jędrychowski <jan.jedrychowski@globalapptesting.com>
2021-06-21 13:59:05 +0200

    Remove createJSModules()

--- android/src/main/java/com/rn/full/screen/FullScreenModule.java
+++ android/src/main/java/com/rn/full/screen/FullScreenModule.java
@@ -32,11 +32,6 @@ public class FullScreenModule implements ReactPackage {
   }
 
   @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
       return Collections.emptyList();
   }
